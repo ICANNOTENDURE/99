5ad2c05ecf1a9c5eb6521bb5c4722859836f89c5
ZDD <ZDD@192.168.1.108>
2017-01-13 07:22:23 +0800

    22

--- src/test/java/com/fh/ArrayTest.java
+++ src/test/java/com/fh/ArrayTest.java
@@ -2,6 +2,7 @@ package com.fh;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -30,7 +31,37 @@ public class ArrayTest {
 		menus.add(new Menu(33, "log", 0));
 		Collections.sort(menus);
 		for(Menu m:menus){
-			System.out.println(m.getId());
+			System.out.println("方式1:"+m.getId());
+			
+		}
+		for(int i=0;i<menus.size();i++){
+			System.out.println("方式2:"+menus.get(i).getId());
+			
+		}
+		Iterator<Menu> it=menus.iterator();
+		while(it.hasNext()){
+			System.out.println("方式3:"+it.next().getId());
 		}
 	}
+	@Test
+	public void testThread() throws InterruptedException{
+			final List<Integer> list=new ArrayList<Integer>();
+			//final List<Integer> list=Collections.synchronizedList(new ArrayList<Integer>());
+			for(int i=0;i<10;i++){
+				list.clear();
+				for(int j=0;j<10000;j++){
+					new Thread(new Runnable() {
+						@Override
+						public void run() { 
+							list.add(2);
+						}
+					}).start();
+				
+				}
+
+				Thread.sleep(10); 
+				System.out.println(list.size());
+			}
+	}
+
 }
