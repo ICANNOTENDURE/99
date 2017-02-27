f428e7a054f98431fc22417d7e3e2289eec27aee
ZDD <ZDD@192.168.1.107>
2017-02-27 11:18:26 +0800

    333

--- src/main/java/com/fh/controller/app/doc/AppdocUserController.java
+++ src/main/java/com/fh/controller/app/doc/AppdocUserController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +22,8 @@ import com.fh.controller.base.BaseController;
 import com.fh.entity.JsonResult;
 import com.fh.entity.system.doc.DocInfo;
 import com.fh.entity.system.doc.DocInfoReq;
+import com.fh.entity.system.doc.DocService;
+import com.fh.entity.system.doc.DocServiceReq;
 import com.fh.entity.system.doc.DocUser;
 import com.fh.plugin.annotation.AppToken;
 import com.fh.service.common.impl.CommonService;
@@ -197,11 +200,7 @@ public class AppdocUserController extends BaseController{
 	}
 	
 	
-	/**
-	 * 医生账户认证
-	 * @return
-	 * @throws Exception 
-	 */
+
 	@AppToken
 	@ApiOperation(notes = "医生账户认证",  value = "医生账户认证")
 	@RequestMapping(value="/saveDocInfo",method = RequestMethod.POST)
@@ -241,4 +240,51 @@ public class AppdocUserController extends BaseController{
 		return new JsonResult<>();
 	}	
 	
+
+	@AppToken
+	@ApiOperation(notes = "医生申请服务",  value = "医生申请服务")
+	@RequestMapping(value="/saveDocService",method = RequestMethod.POST)
+	@ResponseBody
+	public JsonResult<Object> saveDocService(
+			@ApiParam(value = "APP_TOKEN",name="APP_TOKEN") @RequestParam String APP_TOKEN,
+			@ApiParam(value = "服务类型id",name="serviceTypeId") @RequestParam String serviceTypeId,
+			@ApiParam(value = "服务价格",name="price")	@RequestParam BigDecimal price) throws Exception{
+		JsonResult<Object> jsonResult=new JsonResult<Object>();
+		if(StringUtils.isBlank(super.getLoginInfoId())){
+			jsonResult.setCode(2);
+			jsonResult.setMessage("请先认证医生资格");
+			return jsonResult;
+		}
+		DocInfo docInfo=commonService.selectByPrimaryKey(DocInfo.class, getLoginInfoId());
+		if(!AuditDocStatusEnum.AUDIT_PASS.getCode().equals(docInfo.getAuditFlag())){
+			jsonResult.setCode(2);
+			jsonResult.setMessage("请先认证医生资格");
+			return jsonResult;
+		}
+		DocService docService=new DocService();
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("service_Type", serviceTypeId);
+		map.put("docinfo_Id", docInfo.getInfoId());
+		List<DocService> docServices=commonService.selectByEqCon(DocService.class, map);
+		if(docServices.size()>0){
+			if(AuditDocStatusEnum.AUDIT_PASS.getCode().equals(docServices.get(0).getAuditFlag())){
+				jsonResult.setCode(3);
+				jsonResult.setMessage("已经申请成功");
+				return jsonResult;
+			}
+			docService.setServiceId(docServices.get(0).getServiceId());
+		}
+		docService.setServicePrice(price);
+		docService.setDocinfoId(docInfo.getInfoId());
+		docService.setAuditFlag(AuditDocStatusEnum.AUDIT_ING.getCode());
+		docService.setServiceType(serviceTypeId);
+		commonService.saveOrUpdate(docService);
+		DocServiceReq docServiceReq=new DocServiceReq();
+		docServiceReq.setContent("医师申请");
+		docServiceReq.setCreateDate(new Date());
+		docServiceReq.setStatus(AuditDocStatusEnum.AUDIT_ING.getCode());
+		docServiceReq.setServiceId(docService.getServiceId());
+		commonService.saveOrUpdate(docServiceReq);
+		return jsonResult;
+	}	
 }
